45493c3ace9deb0aa006cc7a4dd222f7acd68e05
Daniel Gomez <daniel.gomez@quantco.com>
2025-10-15 17:11:11 -0400

    Attempt to modernize

--- heudiconv/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from ._version import __version__
-from .info import __packagename__
-
-__all__ = ["__packagename__", "__version__"]
-
-lgr = logging.getLogger(__name__)
-lgr.debug("Starting the abomination")  # just to "run-test" logging
